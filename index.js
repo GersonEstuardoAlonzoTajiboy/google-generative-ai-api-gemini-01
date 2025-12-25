64f5a338d6223e58c71d727fdcff6bbefd3a83a9
Gerson Estuardo Alonzo Tajiboy <111700508+GersonEstuardoAlonzoTajiboy@users.noreply.github.com>
2025-12-24 19:21:23 -0600

    Add IDE config files and improve error handling
    Added JetBrains IDE configuration files to the .idea directory for project setup. Updated index.js to improve error handling by catching and logging errors from the run() function.

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 import dotenv from 'dotenv';
-import { GoogleGenerativeAI } from '@google/generative-ai';
+import {GoogleGenerativeAI} from '@google/generative-ai';
 
 dotenv.config();
 
 const generativeAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 async function run() {
-  const modelAI = generativeAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+    const modelAI = generativeAI.getGenerativeModel({model: 'gemini-1.5-flash'});
 
-  const prompt = "Realiza un resumen de mi pelicula favorita Harry Potter y la Cámara de los Secretos";
+    const prompt = "Realiza un resumen de mi pelicula favorita Harry Potter y la Cámara de los Secretos";
 
-  const result = await modelAI.generateContent(prompt);
-  const response = result.response;
-  const text = response.text();
-  console.log(text);
+    const result = await modelAI.generateContent(prompt);
+    const response = result.response;
+    const text = response.text();
+    console.log(text);
 }
 
-run();
+run().catch(console.error);
